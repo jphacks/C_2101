8a46fecd51eb85c22acdd86ad01a649a3dbbeed8
averak <abe12@mccc.jp>
2021-10-23 12:08:18 +0900

    [test] abstract UT/IT interface

---
+++ app/src/test/java/dev/abelab/jphacks/util/AbstractUtil_UT.java
@@ -0,0 +1,17 @@
+package dev.abelab.jphacks.util;
+
+import java.util.Date;
+import dev.abelab.jphacks.annotation.UnitTest;
+
+/**
+ * Abstract Util Unit Test
+ */
+@UnitTest
+public abstract class AbstractUtil_UT {
+
+	static final Integer SAMPLE_INT = 1;
+	static final Date SAMPLE_DATE = new Date();
+	static final String SAMPLE_STR = "SAMPLE";
+	static final Boolean SAMPLE_BOOL = false;
+
+}
