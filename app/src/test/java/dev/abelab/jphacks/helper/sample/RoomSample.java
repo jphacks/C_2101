47f04094794e5fcc617a599d648d0a48a194a2ce
averak <abe12@mccc.jp>
2021-10-23 22:37:15 +0900

    [feat] create sample builders

---
+++ app/src/test/java/dev/abelab/jphacks/helper/sample/RoomSample.java
@@ -0,0 +1,68 @@
+package dev.abelab.jphacks.helper.sample;
+
+import java.util.Date;
+
+import dev.abelab.jphacks.db.entity.Room;
+
+/**
+ * Room Sample Builder
+ */
+public class RoomSample extends AbstractSample {
+
+	public static RoomSampleBuilder builder() {
+		return new RoomSampleBuilder();
+	}
+
+	public static class RoomSampleBuilder {
+
+		private Integer id = SAMPLE_INT;
+		private String title = SAMPLE_STR;
+		private String description = SAMPLE_STR;
+		private Integer ownerId = SAMPLE_INT;
+		private Date startAt = SAMPLE_DATE;
+		private Date finishAt = SAMPLE_DATE;
+
+		public RoomSampleBuilder id(Integer id) {
+			this.id = id;
+			return this;
+		}
+
+		public RoomSampleBuilder title(String title) {
+			this.title = title;
+			return this;
+		}
+
+		public RoomSampleBuilder description(String description) {
+			this.description = description;
+			return this;
+		}
+
+		public RoomSampleBuilder ownerId(Integer ownerId) {
+			this.ownerId = ownerId;
+			return this;
+		}
+
+		public RoomSampleBuilder startAt(Date startAt) {
+			this.startAt = startAt;
+			return this;
+		}
+
+		public RoomSampleBuilder finishAt(Date finishAt) {
+			this.finishAt = finishAt;
+			return this;
+		}
+
+		public Room build() {
+			return Room.builder() //
+				.id(this.id) //
+				.title(this.title) //
+				.description(this.description) //
+				.ownerId(this.ownerId) //
+				.startAt(this.startAt) //
+				.finishAt(this.finishAt) //
+				.build();
+		}
+
+	}
+
+}
