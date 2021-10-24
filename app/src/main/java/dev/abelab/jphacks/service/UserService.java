0020859db4598650adfa0ab06ca4db642a6a49dc
averak <abe12@mccc.jp>
2021-10-24 18:39:07 +0900

    [feat] implement get users api

--- app/src/main/java/dev/abelab/jphacks/service/UserService.java
+++ app/src/main/java/dev/abelab/jphacks/service/UserService.java
@@ -1,6 +1,8 @@
 
 package dev.abelab.jphacks.service;
 
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.modelmapper.ModelMapper;
@@ -9,6 +11,7 @@ import lombok.*;
 import dev.abelab.jphacks.db.entity.User;
 import dev.abelab.jphacks.api.request.LoginUserUpdateRequest;
 import dev.abelab.jphacks.api.request.LoginUserPasswordUpdateRequest;
+import dev.abelab.jphacks.api.response.UsersResponse;
 import dev.abelab.jphacks.repository.UserRepository;
 import dev.abelab.jphacks.logic.UserLogic;
 import dev.abelab.jphacks.util.AuthUtil;
@@ -34,6 +37,24 @@ public class UserService {
     @Transactional
     public UserResponse getLoginUser(final User loginUser) {
         return this.modelMapper.map(loginUser, UserResponse.class);
+    }
+
+    /**
+     * ユーザ一覧を取得
+     *
+     * @param loginUser ログインユーザ
+     *
+     * @return ユーザ一覧レスポンス
+     */
+    @Transactional
+    public UsersResponse getUsers(final User loginUser) {
+        // ユーザ一覧の取得
+        final var users = this.userRepository.findAll();
+        final var userResponses = users.stream() //
+            .map(user -> this.modelMapper.map(user, UserResponse.class)) //
+            .collect(Collectors.toList());
+
+        return new UsersResponse(userResponses);
     }
 
 }
