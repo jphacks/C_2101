9fcbe45851da832408cf42776a814ae66e5b8f2a
averak <abe12@mccc.jp>
2021-10-29 00:53:59 +0900

    [feat] create home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,14 @@
 import { Text, Button } from "@chakra-ui/react";
 import React from "react";
+import {
+  Box,
+  Heading,
+  Container,
+  Stack,
+  Icon,
+  useColorModeValue,
+  createIcon,
+} from "@chakra-ui/react";
 import Layout from "../components/layout";
 import { useLogin } from "../hooks/useLogin";
 import NextLink from "next/link";
@@ -13,14 +22,54 @@ const Home: React.VFC = () => {
 
   return (
     <Layout>
-      <Text>Home</Text>
-      <NextLink href={"/login"} passHref>
-        <Button onClick={handleClickLogout}>Login</Button>
-      </NextLink>
-      <Button onClick={handleClickLogout}>Logout</Button>
-      {/* Authorizationの表示はエラー出るけど、表示しなければ良い */}
-      <Text>header: {authHeader?.Authorization}</Text>
-      <Text>user: {user?.name}</Text>
+      <Container maxW={"3xl"}>
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            新しいLT Spaceへようこそ <br />
+            <Text as={"span"} color={"green.400"} fontSize="3rem">
+              video conferencing service
+            </Text>
+          </Heading>
+
+          <Text color={"gray.500"}>
+            LT
+            Spaceはオンライン発表会に特化したクラウドベースなビデオチャットプラットフォームです。
+            <br />
+            コミュニティ内でのLT会や勉強会など、快適なオンライン発表環境を提供します。
+          </Text>
+          <Stack
+            direction={"column"}
+            spacing={3}
+            align={"center"}
+            alignSelf={"center"}
+            position={"relative"}
+          >
+            <Button
+              colorScheme={"green"}
+              bg={"green.400"}
+              rounded={"full"}
+              px={6}
+              _hover={{
+                bg: "green.500",
+              }}
+            >
+              Get Started
+            </Button>
+            <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
+              Learn more
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
     </Layout>
   );
 };
