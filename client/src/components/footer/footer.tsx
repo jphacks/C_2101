e89a4f0a0359abcab33c85aad4958989932999e0
averak <abe12@mccc.jp>
2021-10-29 01:00:48 +0900

    [feat] see you footer bg color

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -9,7 +9,6 @@ import {
 export default function Footer() {
   return (
     <Box
-      bg={useColorModeValue("gray.50", "gray.900")}
       color={useColorModeValue("gray.700", "gray.200")}
       borderTopWidth={1}
       borderStyle={"solid"}
