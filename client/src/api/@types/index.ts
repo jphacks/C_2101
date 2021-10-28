9ea5ef87caa47f8debad2394ad2169e0d235f4e1
averak <abe12@mccc.jp>
2021-10-29 04:09:43 +0900

    [fix] change ..At column to Date

--- client/src/api/@types/index.ts
+++ client/src/api/@types/index.ts
@@ -1,66 +1,66 @@
 /* eslint-disable */
 export type AccessTokenResponse = {
-  accessToken: string
-  tokenType: string
-}
+  accessToken: string;
+  tokenType: string;
+};
 
 export type LoginRequest = {
-  email: string
-  password: string
-}
+  email: string;
+  password: string;
+};
 
 export type LoginUserPasswordUpdateRequest = {
-  currentPassword: string
-  newPassword: string
-}
+  currentPassword: string;
+  newPassword: string;
+};
 
 export type LoginUserUpdateRequest = {
-  email: string
-  icon?: string
-  name: string
-}
+  email: string;
+  icon?: string;
+  name: string;
+};
 
 export type RoomCreateRequest = {
-  description: string
-  finishAt: string
-  startAt: string
-  title?: string
-}
+  description: string;
+  finishAt: Date;
+  startAt: Date;
+  title?: string;
+};
 
 export type RoomJoinRequest = {
-  title: string
-  type: number
-}
+  title: string;
+  type: number;
+};
 
 export type RoomResponse = {
-  description: string
-  finishAt: string
-  id: number
-  owner: UserResponse
-  speakers: UserResponse[]
-  startAt: string
-  title: string
-  viewers: UserResponse[]
-}
+  description: string;
+  finishAt: Date;
+  id: number;
+  owner: UserResponse;
+  speakers: UserResponse[];
+  startAt: Date;
+  title: string;
+  viewers: UserResponse[];
+};
 
 export type RoomsResponse = {
-  rooms: RoomResponse[]
-}
+  rooms: RoomResponse[];
+};
 
 export type SignupRequest = {
-  email: string
-  icon: string
-  name: string
-  password: string
-}
+  email: string;
+  icon: string;
+  name: string;
+  password: string;
+};
 
 export type UserResponse = {
-  email: string
-  iconUrl: string
-  id: number
-  name: string
-}
+  email: string;
+  iconUrl: string;
+  id: number;
+  name: string;
+};
 
 export type UsersResponse = {
-  users: UserResponse[]
-}
+  users: UserResponse[];
+};
