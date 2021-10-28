c7ec34926f2eaf166fd96ec1c4ff55eda0a200d0
averak <abe12@mccc.jp>
2021-10-28 16:58:58 +0900

    [feat] show copyright in footer

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,7 +1,31 @@
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
 export default function Footer() {
   return (
-    <>
-      <footer>Footer</footer>
-    </>
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+      borderTopWidth={1}
+      borderStyle={"solid"}
+      borderColor={useColorModeValue("gray.200", "gray.700")}
+    >
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        py={3}
+        direction={{ base: "column", md: "row" }}
+        spacing={4}
+        justify={{ base: "center", md: "center" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Text>© 2020 LT Space. All rights reserved</Text>
+      </Container>
+    </Box>
   );
 }
