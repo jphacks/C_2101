8ac061c3a2bd3b8dbfe340d44021766cca8e00f2
elecdeer <37701077+elecdeer@users.noreply.github.com>
2021-10-30 03:34:42 +0900

    [refactor] Login移動

---
+++ client/src/components/page/PageLogin.tsx
@@ -0,0 +1,134 @@
+import { useLogin } from "../../hooks/useLogin";
+import { useRouter } from "next/router";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Link,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import Layout from "../layout";
+import NextLink from "next/link";
+import React, { useEffect } from "react";
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+export const PageLogin = () => {
+  const { fetchLogin, user } = useLogin();
+  const router = useRouter();
+  const toast = useToast();
+  const spaceId = router.query.next?.toString();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>();
+
+  const handleClickLogin = async (values: FormData) => {
+    //ここの値はフォームからとる
+    fetchLogin({
+      email: values.email,
+      password: values.password,
+    })
+      .then(() => {
+        toast({
+          title: "ログインに成功しました。",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+        if (!!spaceId) router.push(`/${spaceId}`);
+        else router.push("/explore");
+      })
+      .catch((error: string) => {
+        toast({
+          title: error as string,
+          status: "error",
+          duration: 10000,
+          isClosable: true,
+        });
+      });
+  };
+
+  useEffect(() => {
+    //すでにログイン済み
+    if (user) {
+      void router.push(String(router.query["next"]) ?? "/explore");
+    }
+  }, [router, user]);
+
+  return (
+    <Layout>
+      <Text fontSize="4xl" textAlign="center" marginBottom="12" marginTop="24">
+        LT Spaceにログイン
+      </Text>
+
+      <Stack spacing={4} maxWidth={500} margin="auto">
+        <form onSubmit={handleSubmit(handleClickLogin)}>
+          <FormControl isInvalid={!!errors.email} mt={4}>
+            <FormLabel>メールアドレス</FormLabel>
+            <Input
+              type="email"
+              placeholder="メールアドレス"
+              id="email"
+              {...register("email", {
+                required: "メールアドレスは必須です。",
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: "メールアドレス形式で入力してください。",
+                },
+              })}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.password} mt={4}>
+            <FormLabel>パスワード</FormLabel>
+            <Input
+              type="password"
+              placeholder="パスワード"
+              id="password"
+              {...register("password", {
+                required: "パスワードは必須です。",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+          </FormControl>
+
+          <Stack spacing={10}>
+            <Button
+              mt={8}
+              bg={"teal.400"}
+              color={"white"}
+              _hover={{
+                bg: "teal.500",
+              }}
+              isLoading={isSubmitting}
+              type="submit"
+            >
+              続行する
+            </Button>
+            <Text>
+              LT Spaceを初めて利用する場合 :{" "}
+              <NextLink href={"/signup"} passHref>
+                <Link color="teal.400">無料登録する</Link>
+              </NextLink>
+            </Text>
+          </Stack>
+        </form>
+      </Stack>
+    </Layout>
+  );
+};
