4993a08329b2ae18e33518bb154275ae4f1e39ab
averak <abe12@mccc.jp>
2021-10-29 18:59:21 +0900

    [feat] 参加登録したルームを表示

--- client/src/pages/explore.tsx
+++ client/src/pages/explore.tsx
@@ -18,14 +18,35 @@ import {
   Heading,
 } from "@chakra-ui/react";
 import RoomCard from "../components/room/room-card";
+import { useLogin } from "../hooks/useLogin";
 import { useRoom } from "../hooks/useRoom";
 
 const Explore: React.VFC = () => {
+  const { user } = useLogin();
   const { rooms } = useRoom();
 
-  if (rooms === undefined) {
+  if (!rooms || !user) {
     return <></>;
   }
+
+  // 参加登録したルーム
+  const now = new Date();
+  const joinRooms = rooms.filter((room) => {
+    if (now > room.finishAt) {
+      return false;
+    }
+
+    let result = room.owner.id === user.id;
+
+    room.speakers.forEach(
+      (speaker) => (result = result || speaker.id === user.id)
+    );
+    room.viewers.forEach(
+      (viewer) => (result = result || viewer.id === user.id)
+    );
+
+    return result;
+  });
 
   // 新しいもの順にソート
   rooms.sort((a, b) => {
@@ -36,21 +57,34 @@ const Explore: React.VFC = () => {
     <Layout>
       <Stack maxW={"100vw"}>
         <Stack align={"center"}>
-          <Stack
-            align={"start"}
-            textAlign={"center"}
-            spacing={{ base: 8, md: 14 }}
-            py={10}
-            flex={"center"}
-          >
+          <Stack align={"start"} textAlign={"center"} py={10} flex={"center"}>
             <Box width="750px" borderBottom="4px" borderColor={"teal.400"}>
               <Heading fontSize="1.5rem" textAlign={"start"}>
-                直近開催のイベント
+                参加登録したルーム
+              </Heading>
+            </Box>
+            <Text>
+              {joinRooms.length === 0 ? "参加登録したルームはありません。" : ""}
+            </Text>
+            {joinRooms.map((room) => (
+              <>
+                <RoomCard room={room} key={room.id + 'xx'} />
+                <br />
+              </>
+            ))}
+            <br />
+
+            <Box width="750px" borderBottom="4px" borderColor={"teal.400"}>
+              <Heading fontSize="1.5rem" textAlign={"start"}>
+                新着ルーム
               </Heading>
             </Box>
 
             {rooms.map((room) => (
-              <RoomCard key={room.id} room={room} />
+              <>
+                <RoomCard room={room} key={room.id} />
+                <br />
+              </>
             ))}
           </Stack>
         </Stack>
