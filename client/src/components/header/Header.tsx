60ad062f1c4edc7fd7e1420ffaf37d7b57e9f108
elecdeer <37701077+elecdeer@users.noreply.github.com>
2021-10-30 03:17:40 +0900

    [refactor] ファイル名を変えていきたい

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex, Heading, HStack, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
-import UserMenu from "./user-menu";
+import UserMenu from "./UserMenu";
 
 type Props = {
   contentTitle?: string;
