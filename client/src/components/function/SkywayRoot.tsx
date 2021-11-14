01c470a76961f15e6af94bc170e396efa41d2315
elecdeer <37701077+elecdeer@users.noreply.github.com>
2021-11-14 21:39:39 +0900

    [feat] SkywayRootをファイルだけ作った

---
+++ client/src/components/function/SkywayRoot.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type SkywayRootProps = {
+  children: React.ReactNode;
+};
+
+//videoを表示するコンポーネントがuseEffectでfocusStreamIdをみる
+//credentialはatomで持つ
+//RootコンポーネントでsfuRoomをredでもってuseContextでを引き回すか？
