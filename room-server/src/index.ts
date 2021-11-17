5ed2054e666832395481e8b76ea1a526fdde7aa3
averak <abe12@mccc.jp>
2021-11-18 04:00:03 +0900

    [feat] broadcast

--- room-server/src/index.ts
+++ room-server/src/index.ts
@@ -91,7 +91,15 @@ io.on("connection", (socket) => {
   socket.on("leaveRoom", async () => {
     console.log("[leaveRoom] is called");
 
+    // ユーザ情報を取得
+    const userSession = await userSessionService.getUserSession(socket.id);
+    if (!userSession || userSession.isGuest) {
+      return;
+    }
+
+    // ルームから退出
     await roomSessionService.leaveRoom(socket.id);
+    await socket.leave(String(userSession.roomId));
   });
 
   socket.on("getScreenCredential", async (auth, res) => {
@@ -127,10 +135,17 @@ io.on("connection", (socket) => {
   socket.on("postComment", async (comment) => {
     console.log("[postComment] is called");
 
+    // ユーザ情報を取得
+    const userSession = await userSessionService.getUserSession(socket.id);
+    if (!userSession || userSession.isGuest) {
+      return;
+    }
+
+    // コメントを配信
     await roomSessionService.postComment(comment, socket.id);
+    io.to(String(userSession.roomId)).emit("broadcastComment", comment);
   });
 
-  // TODO
   socket.on("postReaction", async (reaction) => {
     console.log("[postReaction] is called");
 
@@ -140,6 +155,7 @@ io.on("connection", (socket) => {
       return;
     }
 
+    // リアクションを配信
     io.to(String(userSession.roomId)).emit("broadcastReaction", reaction);
   });
 
@@ -151,24 +167,29 @@ io.on("connection", (socket) => {
   socket.on("setTimetable", async (timetable) => {
     console.log("[setTimetable] is called");
 
+    // ユーザ情報を取得
+    const userSession = await userSessionService.getUserSession(socket.id);
+    if (!userSession || userSession.isGuest) {
+      return;
+    }
+
+    // タイムテーブルを更新
     await roomSessionService.setTimetable(timetable, socket.id);
+    io.to(String(userSession.roomId)).emit("updateTimetable", timetable);
   });
 
   socket.on("setTimer", async (timer) => {
     console.log("[setTimer] is called");
 
-    await roomSessionService.setTimer(timer, socket.id);
-  });
-
-  socket.on("getInitialStates", async (res) => {
-    const roomSession = await roomSessionService.getActiveRoomSession(
-      socket.id
-    );
-    if (!roomSession) {
+    // ユーザ情報を取得
+    const userSession = await userSessionService.getUserSession(socket.id);
+    if (!userSession || userSession.isGuest) {
       return;
     }
 
-    res(roomSession);
+    // タイマーを更新
+    await roomSessionService.setTimer(timer, socket.id);
+    io.to(String(userSession.roomId)).emit("updateTimer", timer);
   });
 });
 
